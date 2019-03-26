7259a3489a3a3699f2a9e02ff6758d782276e053
HugoCapocci <h.capocci@gmail.com>
2019-03-26 21:29:03 +0100

    Add promisify

--- copy.js
+++ copy.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 const originalMap = path.join('data', 'map.pdf');
 const mapCopy = 'map-copy.pdf';
 
-fs.readFile(originalMap, (err, data) => {
-  if (err) throw err;
-  console.log(data);
-  fs.writeFile(mapCopy, data, (err) => {
-    if (err) throw err;
+readFile(originalMap)
+  .then((data) => {
+    console.log(data);
+    return writeFile(mapCopy, data);
+  })
+  .then(() => {
     console.log('fichier copié!');
+  })
+  .catch((err) => {
+    console.log('une erreur est survenue: ', err);
   });
-});
