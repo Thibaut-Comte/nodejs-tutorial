b37b87f46507a4f4107f2a02a01459400772510d
HugoCapocci <h.capocci@gmail.com>
2019-03-27 21:45:46 +0100

    implements POST /api/v1/message

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var bodyParser = require("body-parser");
 const app = express();
 const v1 = express.Router();
 
 const MessageService = require('./services/message');
 const messageService = new MessageService();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api/v1', v1);
 
 v1.get('/message', (request, response) => {
@@ -18,6 +21,15 @@ v1.get('/message/:id', (request, response) => {
     response.send(
         messageService.getMessage(id)
     );
+});
+
+v1.post('/message', (request, response) => {
+    const message = request.body;
+    try {
+        response.send(messageService.createMessage(message));
+    } catch (error) {
+        response.sendStatus(400).end(error);
+    }
 });
 
 app.listen(3000, () => {
