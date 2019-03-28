b579610c75e0daae592ad83f8030ff2a9b5a95ab
hcapocci <hcapocci@talentsoft.com>
2019-03-28 18:07:28 +0100

    Decode authentification data from header and authenticate user

--- middleware/basic-auth.js
+++ middleware/basic-auth.js
@@ -1,12 +1,15 @@
+const { login } = require('../services/authentication');
+
 module.exports.basicAuth = (request, response, next) => {
     const authorization = request.headers.authorization;
-    // decode base 64
+    if (authorization) {
+        // decode base 64
+        const decoded = Buffer.from(authorization.replace('Basic ', ''), 'base64');
+        const [username, password] = decoded.toString('utf8').split(':');
 
-    // try to authenticate
-
-    // return 401 if denied
+        // try to authenticate
+        if (login(username, password)) return next();
+    }
     response.sendStatus(401)
         .end('Access denied');
-    // otherwise
-    // next();
 }
