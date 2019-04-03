6242d5ca72c4926513bc11e60d76d035f3d49591
Thibaut-Comte <thibs176@gmail.com>
2019-04-03 11:07:53 +0200

    .env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const db = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -9,6 +11,24 @@ const messageService = new MessageService();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/v1', v1);
+
+
+const basicAuth = (request, response, next) => {
+    const authorization = request.headers.authorization;
+    console.log(authorization);
+    //On récupère la partie 1 donc la 2nde ici le mdp en base64
+    const decoded = Buffer.from(authorization.split(" ")[1], 'base64').toString('utf8');
+
+    const login = decoded.split(":")[0];
+    const pwd = decoded.split(":")[1];
+
+    if (login == "test" && pwd == "password") {
+        return next();
+    } else {
+        response.sendStatus(401).end();
+
+    }
+};
 
 v1.get('/message', (request, response) => {
     messageService.getMessages()
@@ -23,7 +43,7 @@ v1.get('/message/:id', (request, response) => {
         message ? response.send(message) : response.sendStatus(404);
     });
 });
-v1.post('/message',  (request, response) => {
+v1.post('/message', basicAuth,  (request, response) => {
     const message = request.body;
     messageService.insertMessage(message)
     .then(result => {
@@ -35,7 +55,7 @@ v1.post('/message',  (request, response) => {
     });
 });
 
-v1.put('/message/:id',  (request, response) => {
+v1.put('/message/:id', basicAuth,  (request, response) => {
     const id = request.params.id;
     const message = {...request.body, id:id};
     messageService.updateMessage(message, id)
@@ -48,7 +68,7 @@ v1.put('/message/:id',  (request, response) => {
     });
 });
 
-v1.delete('/message/:id',  (request, response) => {
+v1.delete('/message/:id', basicAuth,  (request, response) => {
     const id = request.params.id;
     messageService.deleteMessage(id)
     .then(() => {
@@ -59,6 +79,6 @@ v1.delete('/message/:id',  (request, response) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000!');
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server listening on port :',process.env.APP_PORT);
 });
