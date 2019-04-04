de923e8c5b65d9ae2dc4b0afa441737ce887d291
Hugo CAPOCCI <h.capocci@gmail.com>
2019-04-05 00:03:27 +0200

    First step, two-way websocket, monoclient

--- web/websocket.js
+++ web/websocket.js
@@ -4,19 +4,21 @@ const webSocketServer = new WebSocket.Server({ port: 3001 });
 
 webSocketServer.on('connection', webSocket => {
     console.log('connection established');
-    webSocket.onmessage = message => {
-        console.log(`Received message => ${message}`)
+    webSocket.send("Welcome!");
+    webSocket.onmessage = messageEvent => {
+        console.log('Message received:', messageEvent.data);
+        const message = messageEvent.data;
+        webSocket.send(message);
     };
     webSocket.onopen = event => {
         console.log('socket opened by client ', event);
+        webSocket.send(messages);
     };
     webSocket.onerror = error => {
         console.error('WebSocket error occurs: ', error);
     };
     webSocket.onclose = event => {
         console.error('WebSocket connection closed by client ', event); 
-    }
-
-    webSocket.send('message from back-end!');
+    };
 });
 
