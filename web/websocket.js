92842b8f0455ad27e47156b281bbc2bd21a2c878
Hugo CAPOCCI <h.capocci@gmail.com>
2019-04-28 21:39:14 +0200

    add websocket tests

--- web/websocket.js
+++ web/websocket.js
@@ -1,20 +1,10 @@
 const WebSocket = require('ws')
 
 const webSocketServer = new WebSocket.Server({ port: 3001 });
-
-webSocketServer.broadcast = function broadcast(data) {
-    webSocketServer.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-};
   
 webSocketServer.on('connection', webSocket => {
-    console.log('connection established');
     webSocket.send('Welcome!');
     webSocket.onmessage = messageEvent => {
-        console.log('Message received:', messageEvent.data);
         const message = messageEvent.data;
         webSocket.send(message);
         webSocketServer.clients.forEach(function each(client) {
@@ -23,13 +13,6 @@ webSocketServer.on('connection', webSocket => {
             }
         });
     };
-    webSocket.onopen = event => {
-        console.log('socket opened by client ', event);
-    };
-    webSocket.onerror = error => {
-        console.error('WebSocket error occurs: ', error);
-    };
-    webSocket.onclose = event => {
-        console.error('WebSocket connection closed by client ', event); 
-    };
 });
+
+module.exports = webSocketServer;
