41de918f440415d962895d44d3179e6f06eec10a
HugoCapocci <h.capocci@gmail.com>
2019-04-29 11:17:33 +0200

    Refactoring fileService using async/await

--- services/file.js
+++ services/file.js
@@ -8,38 +8,29 @@ module.exports = class FileService {
     this.pool = new Pool();
   }
 
-  openTransaction() {
+  async openTransaction() {
+    const client = await this.pool.connect();
+    await client.query('BEGIN');
+    return client;
+  }
+
+  async validateTransaction(client) {
+    await client.query('COMMIT');
+    return client.release();
+  }
+
+  async abortTransaction(client) {
+    await client.query('ROLLBACK');
+    return client.release();
+  }
+
+  async saveFileInfos(fileInfo) {
     let client;
-    return this.pool.connect()
-    .then(connectedClient => {
-      client = connectedClient;
-      return client.query('BEGIN');
-    })
-    .then(() => client);
-  }
-
-  validateTransaction(client) {
-    return client.query('COMMIT')
-    .then(() => {
-      client.release();
-    });
-  }
-
-  abortTransaction(client) {
-    return client.query('ROLLBACK')
-    .then(() => {
-      client.release();
-    });
-  }
-
-  saveFileInfos(fileInfo) {
-    let client;
-    return this.openTransaction()
-    .then(connectedClient => {
-      client = connectedClient;
-      return client.query(
+    try {
+      client = await this.openTransaction()
+      await client.query(
         `INSERT INTO filestore("file-name", "mime-type", "original-name", size, encoding)
-         VALUES ($1, $2, $3, $4, $5)`,
+        VALUES ($1, $2, $3, $4, $5)`,
         [
           fileInfo.filename,
           fileInfo.mimetype,
@@ -48,85 +39,59 @@ module.exports = class FileService {
           fileInfo.encoding
         ]
       );      
-    })
-    .then(() => {
       return this.validateTransaction(client);
-    })
-    .catch(err => {
+    } catch(err) {
       console.log('error occurs: ', err);
-      return this.abortTransaction(client)
-      .then(() => {
-        return unlink('data/upload/' + fileInfo.filename);
-      })
-      .then(() => Promise.reject(err));
-    });
+      await this.abortTransaction(client)
+      await unlink('data/upload/' + fileInfo.filename);
+      return Promise.reject(err); // or throw err
+    }
   }
 
-  getFileInfos() {
-    let client;
-    return this.pool.connect()
-    .then(connectedClient => {
-      client = connectedClient;
-      return client.query(
-        'SELECT id, "file-name", "mime-type", "original-name", size, encoding FROM filestore;'
-      );      
-    })
-    .then((result) => {
-      client.release();
-      return result.rows;
-    });
+  async getFileInfos() {
+    const client = await this.pool.connect();
+    const result = await client.query(
+      'SELECT id, "file-name", "mime-type", "original-name", size, encoding FROM filestore;'
+    );      
+    client.release();
+    return result.rows;
   }
 
-  getFile(id) {
-    let client;
-    return this.pool.connect()
-    .then(connectedClient => {
-      client = connectedClient;
-      return client.query(
-        'SELECT "file-name", "mime-type", "original-name", size, encoding FROM filestore WHERE id=$1',
-        [id]
-      );
-    })
-    .then(result => {
-      client.release();
-      if (result.rows.length === 0) return Promise.reject('no result');
-      const fileInfo = result.rows[0];
-      const fileReadStream = fs.createReadStream(
-        __dirname + '/../data/upload/' + fileInfo['file-name']
-      );
-      return { fileReadStream, fileInfo };
-    });
+  async getFile(id) {
+    const client = await this.pool.connect();
+    const result = await client.query(
+      'SELECT "file-name", "mime-type", "original-name", size, encoding FROM filestore WHERE id=$1',
+      [id]
+    );
+    client.release();
+    if (result.rows.length === 0) return Promise.reject('no result');
+    const fileInfo = result.rows[0];
+    const fileReadStream = fs.createReadStream(
+      __dirname + '/../data/upload/' + fileInfo['file-name']
+    );
+    return { fileReadStream, fileInfo };
   }
 
-  deleteFile(id) {
+  async deleteFile(id) {
     let client;
     let fileName;
-    return this.openTransaction()
-    .then(connectedClient => {
-      client = connectedClient;
-      return client.query(
+    try {
+      client = await this.openTransaction()
+      const { rows } = await client.query(
         'SELECT "file-name" FROM filestore WHERE id=$1',
         [id]
       );
-    })
-    .then(({ rows }) => { 
       if (rows.length === 0) return Promise.reject('no result');
       fileName = rows[0]['file-name'];
-      return client.query(
+      await client.query(
         'DELETE FROM filestore WHERE id=$1',
         [id]
       );
-    })
-    .then(() => {
-      return unlink(__dirname + '/../data/upload/' + fileName);
-    })
-    .then(() => {
+      await unlink(__dirname + '/../data/upload/' + fileName);
       return this.validateTransaction(client);
-    })
-    .catch((err) => {
-      return this.abortTransaction(client)
-      .then(() => Promise.reject(err));
-    });
+    } catch(err) {
+      await this.abortTransaction(client);
+      return Promise.reject(err);
+    }
   }
-
 }
